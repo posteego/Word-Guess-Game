330b9334e41c406136ad272191b2d7518ca67627
kpostigo <7651554+kpostigo@users.noreply.github.com>
2018-06-25 17:41:57 -0400

    added txt files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,14 +4,24 @@
 /*        CSS changes based on theme choice,             */
 /*        questions are asked based on choice            */
 
+fetch("../themes/astro_words.txt")
+  .then( r => r.text() )
+  .then( t => console.log(t))
+
+// variable declarations
+
+var words = ['test','new'],   // list of words
+  def = ['apple', 'works'],   // definitions
+  wins = 0,                   // win counter
+  remaining = 10,             // # guesses
+  guesses = [];               // letters guessed
+  
+
 // create Dictionary object for different themes
 function Dictionary(word, def) {
   this.word = word;
   this.def = def;
-};
-
-var words = ['mic','new','another'],   // list of words
-  def = ['check','entry','one'];      // definitions
+}
 
 var astronomy = new Dictionary(words,def); // 'astronomy' list
 
